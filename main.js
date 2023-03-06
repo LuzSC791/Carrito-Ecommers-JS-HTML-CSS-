04a602907d33683ad0f68bbd0aa15cd8b1dc072e
LuzSC791 <luzsuarez791@gmail.com>
2023-03-06 09:27:49 -0600

    Add-payProducts

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const catalogue = document.querySelector('.catalogue');
 const productSelect = document.querySelector('.product');
 const navOption = document.querySelector('.nav-option');
-const pay = document.querySelector('.pay-total');
+const pay = document.querySelector('.pay');
+
+let arrAux = [];
+
 const api = async() =>{
     try {
             const url = await fetch('https://api.escuelajs.co/api/v1/products')
             const products = await(url.json())
-            let aux;
-            products.map( (pro,i) =>{
+            products.map((pro,i) =>{
                 let divProduct = document.createElement('div')
                 let dataProduct = document.createElement('div')
                 let divSelect = document.createElement('div') 
@@ -16,6 +18,10 @@ const api = async() =>{
                 let imgProduct = document.createElement('img')
                 let buttonTrolley = document.createElement('button')
                 pro['status'] = true;
+                pro['delete'] = false;
+                pro['totalProduct'] = pro.price;
+                pro['pay'] = [pro.totalProduct];
+
                 dataProduct.innerHTML = (`<p><b>Precio:</b> ${pro.price}</p> <p><b>Categoria:</b> ${pro.category.name}</p> <p><b>Desacripcion:</b> ${pro.description}</p> <p> <b>Folio:</b> ${pro.id}</p>`)
                 imgProduct.setAttribute('src',pro.images)
                 divProduct.appendChild(imgProduct)
@@ -24,53 +30,59 @@ const api = async() =>{
                 dataProduct.classList.add('data-product')
                 divProduct.classList.add('container-product')
                 divProduct.appendChild(dataProduct)
-                buttonTrolley.innerHTML =`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10 20.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm6.304-17l-3.431 14h-2.102l2.541-11h-16.812l4.615 13h13.239l3.474-14h2.178l.494-2h-4.196z"/></svg>`
-
+                
                 buttonTrolley.classList.add('btn-trolley')
                 divProduct.appendChild(buttonTrolley)
                 catalogue.appendChild(divProduct)
+                
+                buttonTrolley.innerHTML =`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10 20.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm6.304-17l-3.431 14h-2.102l2.541-11h-16.812l4.615 13h13.239l3.474-14h2.178l.494-2h-4.196z"/></svg>`
+
                 divProduct.addEventListener('click', () =>{
-                    productSelect.innerHTML = "";
-                    imgSelect.setAttribute('src',products[i].images)
-                    divSelect.innerHTML = (`<p><b>Precio:</b> ${products[i].price}</p> <p><b>Categoria:</b> ${products[i].category.name}</p> <p><b>Desacripcion:</b> ${products[i].description}</p> <p> <b>Folio:</b> ${products[i].id}</p>`)
                     productSelect.appendChild(imgSelect) 
                     productSelect.appendChild(divSelect)
                     imgSelect.classList.add('img-productSelect')
                     divSelect.classList.add('div-select')
+
+                    imgSelect.setAttribute('src',products[i].images)
+                    divSelect.innerHTML = (`<p><b>Precio:</b> ${products[i].price}</p> <p><b>Categoria:</b> ${products[i].category.name}</p> <p><b>Desacripcion:</b> ${products[i].description}</p> <p> <b>Folio:</b> ${products[i].id}</p>`)
+                  
                     btnTrolleySelect.innerHTML =`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10 20.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm6.304-17l-3.431 14h-2.102l2.541-11h-16.812l4.615 13h13.239l3.474-14h2.178l.494-2h-4.196z"/></svg>`
                     btnTrolleySelect.classList.add('btn-trolleyS')
                     productSelect.appendChild(btnTrolleySelect);
                 });
-                buttonTrolley.addEventListener('click', () =>{
+                /////******** */
+                products[i].status == false ? buttonTrolley.disabled = true : buttonTrolley.disabled = false;
+                products[i].status == false ? btnTrolleySelect.disabled = true : btnTrolleySelect.disabled = false;
+                /////******** */
 
-                    const id = pro.id;
-                    count = 0
-                    pushArr(i,products,count)
+                buttonTrolley.addEventListener('click', () =>{
+                    pushArr(i,products)
                     products[i].status == true ? buttonTrolley.disabled = false : buttonTrolley.disabled = true;
                     products[i].status == true ? btnTrolleySelect.disabled = false : btnTrolleySelect.disabled = true;
                     pro.productAdd = 1;
+                    const price = pro.price;
+                    pro.totalProduct = pro.productAdd * price;
                 });
-                let count = 0;
+
                 btnTrolleySelect.addEventListener('click', () =>{
-                    const obj = pro.id;
-                    console.log(`rdtr: ${id}`)
-                    count = 0
-                    pushArr(i,products,count,obj)
+                    pushArr(i,products)
                     products[i].status == true ? buttonTrolley.disabled = false : buttonTrolley.disabled = true;
                     products[i].status == true ? btnTrolleySelect.disabled = false : btnTrolleySelect.disabled = true;
-                    console.log(pro.payProduct)
+                    pro.productAdd = 1;
+                    const price = pro.price;
+                    pro.totalProduct = pro.productAdd * price;
                 });
                 
             });
             
-            let arrAux = [];
-            let arrTotal = []
+            let arrTotal = [];
             const pushArr = (i,products) =>{
+                
                 const options = document.createElement('div'); 
                 options.classList.add('options');
                 const totalElement = document.createElement('p'); 
                 totalElement.classList.add('total-element');
-                const totalPay = document.createElement('p'); 
+                const totalPay = document.createElement('div'); 
                 totalPay.classList.add('total-pay');
                 const action = document.createElement('div'); 
                 action.classList.add('btn-action');
@@ -86,45 +98,33 @@ const api = async() =>{
                 btnDelete.classList.add('btn-delete')
                 divTotal = document.createElement('div');
                 divTotal.classList.add('total')
+
                 
                 arrAux =[...arrAux,i];
-                const addTrolley = arrAux.map ((e)=>{
+                let addTrolley = arrAux.map ((e)=>{
                     products[e].status = false;
                     return products[e];
                 });
-                let deleteTrolley = []
-                btnDelete.addEventListener('click', () =>{
-                    // let r = addTrolley[i].id
-                    const clk = i+1;
-                    // const t = 1
-                    // const aux = addTrolley.indexOf('');
-                    // const index = addTrolley.indexOf(0);
-                    // if (index > -1) {
-                    //     addTrolley.splice(index, 1);
-                    //   }
-                    console.log(i);
-                    // console.log(addTrolley.length);
-                    // console.log(index);
+                
+                const index = addTrolley.length
+                btnDelete.addEventListener('click',() =>{
 
-                    deleteTrolley = addTrolley.slice(clk,1)
-
-                    // deleteTrolley = addTrolley.filter( e => e.id != e[i+1].id)
-
-                    // deleteTrolley = addTrolley.splice(index, 1);
-                    console.log(deleteTrolley);
-                    // console.log(addTrolley);
-                    // console.log(addTrolley);
-                    
+                    products[i].delete == false ? products[i].delete = true : products[i].delete = false;
+                    contOptions.innerHTML = " ";
+                    products[i].totalProduct = 0;
+                    arrTotal[index-1] = products[i].totalProduct; 
+                    const result = arrTotal.reduce((acm, cv) => acm + cv,0);  
+                    pay.innerHTML = " ";
+                    pay.innerHTML = `<span><b>TOTAL: $ ${result}</b></span>`;
                 });
-                console.log(`AVER${addTrolley}`);
-                console.log(addTrolley);
 
+               addTrolley = addTrolley.filter(e => {
+                    return e.delete === false});
                 addTrolley.map((tro,i) =>{
                     imgOption.setAttribute('src',tro.images);
                     btnDelete.innerHTML = `<svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m20.015 6.506h-16v14.423c0 .591.448 1.071 1 1.071h14c.552 0 1-.48 1-1.071 0-3.905 0-14.423 0-14.423zm-5.75 2.494c.414 0 .75.336.75.75v8.5c0 .414-.336.75-.75.75s-.75-.336-.75-.75v-8.5c0-.414.336-.75.75-.75zm-4.5 0c.414 0 .75.336.75.75v8.5c0 .414-.336.75-.75.75s-.75-.336-.75-.75v-8.5c0-.414.336-.75.75-.75zm-.75-5v-1c0-.535.474-1 1-1h4c.526 0 1 .465 1 1v1h5.254c.412 0 .746.335.746.747s-.334.747-.746.747h-16.507c-.413 0-.747-.335-.747-.747s.334-.747.747-.747zm4.5 0v-.5h-3v.5z" fill-rule="nonzero"/> </svg>`
                     btnAdd.innerHTML = `<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M11.5 0c6.347 0 11.5 5.153 11.5 11.5s-5.153 11.5-11.5 11.5-11.5-5.153-11.5-11.5 5.153-11.5 11.5-11.5zm0 1c5.795 0 10.5 4.705 10.5 10.5s-4.705 10.5-10.5 10.5-10.5-4.705-10.5-10.5 4.705-10.5 10.5-10.5zm.5 10h6v1h-6v6h-1v-6h-6v-1h6v-6h1v6z"/></svg>`
                     btnLess.innerHTML = `<svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12.002 2.005c5.518 0 9.998 4.48 9.998 9.997 0 5.518-4.48 9.998-9.998 9.998-5.517 0-9.997-4.48-9.997-9.998 0-5.517 4.48-9.997 9.997-9.997zm0 1.5c-4.69 0-8.497 3.807-8.497 8.497s3.807 8.498 8.497 8.498 8.498-3.808 8.498-8.498-3.808-8.497-8.498-8.497zm4.253 7.75h-8.5c-.414 0-.75.336-.75.75s.336.75.75.75h8.5c.414 0 .75-.336.75-.75s-.336-.75-.75-.75z" fill-rule="nonzero"/></svg>`
-                    // console.log(addTrolley)
                     
                     options.innerHTML = (`<p><b>Precio $</b> ${tro.price}</p> <p><b>Desacripcion:</b> ${tro.description}</p>`);
 
@@ -136,54 +136,54 @@ const api = async() =>{
                     action.appendChild(btnAdd)
                     action.appendChild(btnLess)
                     navOption.appendChild(contOptions);
-                });
-
-                const id = addTrolley.length;
-                if(arrTotal[i] === undefined){
-                    arrTotal[i] = 1
-                }
-                console.log(addTrolley)
-                btnAdd.addEventListener('click',()=>{
-                    // console.log(`I: ${addTrolley.length}`)
-                    arrTotal[i] === 1 ? arrTotal[i] = 2 : arrTotal[i]++;
-                    // console.log(arrTotal)
-                    let auxTotal = 0;
-                    let auxTro
-                    arrTotal.map((tro,ind) =>{
-                        auxTro = tro * addTrolley[ind].price;
-                        totalElement.innerHTML = `<p><b>$ ${auxTro}</b> - ${tro} U`;
-                        auxTotal += auxTro;
-                        totalPay.innerHTML = " ";
-                        totalPay.innerHTML = `<p><b>TOTAL: $ ${auxTotal}</b>`;
-                        console.log(auxTotal)
-                    });
-                });
+                    navOption.appendChild(totalPay);
+                }); 
 
                 
+                btnAdd.addEventListener('click',()=>{
+                    const price = products[i].price;
+                    products[i].productAdd++;
+
+                    products[i].totalProduct = products[i].productAdd * price;
+                    totalElement.innerHTML = `<p><b>$ ${products[i].totalProduct}</b> - ${products[i].productAdd} U`;
+                    arrTotal[index-1] = products[i].totalProduct; 
+                    const result = arrTotal.reduce((acm, cv) => acm + cv,0);  
+                    console.log(result);
+                    pay.innerHTML = " ";
+                    pay.innerHTML = `<span><b>TOTAL: $ ${result}</b></span>`;
+                });
+
+                let auxTotal = 0;
+                let result = addTrolley.map(e =>{
+                    return auxTotal += e.totalProduct
+                });
+                result.reverse()
+                result = result[0]
+                pay.innerHTML = " ";
+                pay.innerHTML = `<span><b>TOTAL: $ ${result}</b></span>`;
 
                 btnLess.addEventListener('click',()=>{
-                    const id = addTrolley.length;
-                    console.log(`Antes: ${arrTotal}`)
-                    arrTotal[i]  === 0 ? delete addTrolley[i] : arrTotal[i]--;
-                    addTrolley[i].productAdd--;
-                    // console.log(arrTotal)
-                    // console.log(`Despues: ${arrTotal}`)
-                    // console.log(arrTotal)
-                    let auxTotal = 0;
-                    let auxTro
-                    arrTotal.map((tro,ind) =>{
-                        auxTro = tro * addTrolley[ind].price;
-                        totalElement.innerHTML = `<p><b>$ ${auxTro}</b> - ${tro} U`;
-                        auxTotal += auxTro;
-                        totalPay.innerHTML = " ";
-                        totalPay.innerHTML = `<p><b>TOTAL: $ ${auxTotal}</b>`;
-                        console.log(auxTotal)
-                    });;
+                    const price = products[i].price;
+                    products[i].productAdd--;
+                    
+                     /////******** */
+
+                    if(products[i].productAdd == 0)
+                        buttonTrolley.disabled = true
+                /////******** */
+
+                    products[i].totalProduct = products[i].productAdd * price;
+                    totalElement.innerHTML = `<p><b>$ ${products[i].totalProduct}</b> - ${products[i].productAdd} U`;
+
+                    if(products[i].productAdd === 0){
+                        products[i].delete == false ? products[i].delete = true : products[i].delete = false;
+                        contOptions.innerHTML = " ";
+                    }
+                    arrTotal[index-1] = products[i].totalProduct; 
+                    const result = arrTotal.reduce((acm, cv) => acm + cv,0);  
+                    pay.innerHTML = " ";
+                    pay.innerHTML = `<span><b>TOTAL: $ ${result}</b></span>`;
                 });
-
-                
-
-                navOption.appendChild(totalPay);
             }            
             let opc = document.querySelector('.ec-trolley');
             let nav = document.querySelector('.nav-option');
@@ -195,7 +195,6 @@ const api = async() =>{
                     : nav.style.display = "none"
             });
 
-            
     } catch (err) {
         console.log(err)
     }
